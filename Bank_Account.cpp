835bacb40618502e269c6323aa1c575a5b96990e
christopherwilson1 <wilso235@uni.coventry.ac.uk>
2017-07-27 14:49:06 +0100

    Now saves newly created accounts to a text file
    This newest updates now saves newly created customer accounts to a text file instead of what it used to do which was delete the account once the program closes. This will allow for other functions such as edit_account to actually do something as they have a actual account to edit.

--- Bank_Account.cpp
+++ Bank_Account.cpp
@@ -20,4 +20,19 @@ void Bank_Account::account_creation()
     std::cout<<"enter in the amount you would like to put in your account"<<std::endl;
     std::cin>>deposit;
     std::cout<<"congratulations your account has been created"<<std::endl;
+    accountFile.open("customer_account.txt");
+    if(accountFile.is_open())
+            {
+                std::cout<<"account saved"<<std::endl;
+                accountFile<<customer_name;
+                accountFile<<account_num
+                accountFile<<account_type
+                accountFile<<deposit
+
+            }
+            else
+            {
+                std::cout<<"information failed to save please seek out a member of staff"<<std::endl;
+            }
+accountFile.close();
 }
