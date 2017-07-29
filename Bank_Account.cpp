1f4cacc83b6611237d6649712cdf6ae1203fa65b
christopherwilson1 <wilso235@uni.coventry.ac.uk>
2017-07-29 15:35:21 +0100

    fixed the txt file
    fixed the class creation function so now a new account doesn't delete and old account and also made it so its handled in the actual class not in a function in main so i can get rid of that just gotta fix the withdrawal so it also appends the customer account.

--- Bank_Account.cpp
+++ Bank_Account.cpp
@@ -1,100 +1,108 @@
 #include "Bank_Account.h"
+using namespace std;
 Bank_Account::Bank_Account()
 {
-    //ctor
+ account_creation();
 }
 
 Bank_Account::~Bank_Account()
 {
-    //dtor
+ //dtor
 }
 void Bank_Account::account_creation()
 {
-    std::cout<<"Enter in your first and last names"<<std::endl;
-    std::getline(std::cin,customer_name);
-    std::cout<<"Enter in the account number"<<std::endl;
-    std::cin>>account_num;
-    std::cout<<"enter in the account type current or savings"<<std::endl;
-    std::cin>>account_type;
-    std::cout<<"enter in the amount you would like to put in your account"<<std::endl;
-    std::cin>>deposit;
-    std::cout<<"congratulations your account has been created saving information"<<std::endl;
-   /* std::ofstream accountFile;
-    accountFile.open("customer_account.txt");
-    if(accountFile.is_open())
-            {
-                std::cout<<"account saved"<<std::endl;
-                accountFile<<customer_name<<std::endl;
-                accountFile<<account_num<<std::endl;
-                accountFile<<account_type<<std::endl;
-                accountFile<<deposit<<std::endl;
+     std::cout << "Enter in your first and last names" << std::endl;
+     std::getline(std::cin, customer_name);
+     std::cout << "Enter in the account number" << std::endl;
+     std::cin >> account_num;
+     std::cout << "enter in the account type current or savings" << std::endl;
+     std::cin >> account_type;
+     std::cout << "enter in the amount you would like to put in your account" << std::endl;
+     std::cin >> deposit;
+     std::cout << "congratulations your account has been created saving information" << std::endl;
+     std::ofstream accountFile;
+     accountFile.open("customer_account.txt",std::fstream::app);
+         if(accountFile.is_open())
+             {
+                 std::cout<<"account saved"<<std::endl;
+                 accountFile<<customer_name<<std::endl;
+                 accountFile<<account_num<<std::endl;
+                 accountFile<<account_type<<std::endl;
+                 accountFile<<deposit<<std::endl;
+                 accountFile<<"" << endl;
 
-            }
-            else
-            {
+             }
+         else
+             {
                 std::cout<<"information failed to save please seek out a member of staff"<<std::endl;
-            }
-accountFile.close();*/
+             }
+ accountFile.close();
 }
 
 void Bank_Account::withdraw_money(double money)
 {
-    string answer;
-    std::cout<<"would you like a receipt y/n"<<std::endl;
+ string answer;
+ std::cout << "would you like a receipt y/n" << std::endl;
 
-    std::cin>>answer;
-    std::ofstream receiptFile;
-    receiptFile.open("customer_receipt.txt");
-    //std::ofstream accountFile;
-    //accountFile.open("customer_account.txt");
-    if(answer=="y")
-    {
+ std::cin >> answer;
+ std::ofstream receiptFile;
+ receiptFile.open("customer_receipt.txt");
+ //std::ofstream accountFile;
+ //accountFile.open("customer_account.txt");
+ if (answer == "y")
+ {
 
-        if(money<=deposit && money >=0 )
-        {
-            deposit-=money;
-            if(receiptFile.is_open())
-            {
-                std::cout<<"receipt printed"<<std::endl;
-                receiptFile<<deposit<<std::endl;
-                //accountFile<<deposit<<std::endl;
-            }
-            else
-            {
-                std::cout<<"receipt failed to print please seek out a member of staff"<<std::endl;
-            }
-        }
-        else if (money<0)
-        {
-            std::cout<<"That is not a figure than can be withdraw please use whole positive numbers"<<std::endl;
-            std::cout<<"The withdrawal has been aborted"<<std::endl;
-        }
-        else if(money>deposit)
-        {
-            std::cout<<"You don't have that much money"<<std::endl;
-            std::cout<<"The withdrawal has been aborted"<<std::endl;
-        }
-    }
-    else
-    {
-        if(money<=deposit && money >=0 )
-        {
-            deposit-=money;
-           // accountFile<<deposit;
-        }
-        else if (money<0)
-        {
-            std::cout<<"That is not a figure than can be withdraw please use whole positive numbers"<<std::endl;
-            std::cout<<"The withdrawal has been aborted"<<std::endl;
-        }
-        else if(money>deposit)
-        {
-            std::cout<<"You don't have that much money"<<std::endl;
-            std::cout<<"The withdrawal has been aborted"<<std::endl;
-        }
-    }
+  if (money <= deposit && money >= 0)
+  {
+   deposit -= money;
+   if (receiptFile.is_open())
+   {
+    std::cout << "receipt printed" << std::endl;
+    receiptFile << deposit << std::endl;
+    //accountFile<<deposit<<std::endl;
+   }
+   else
+   {
+    std::cout << "receipt failed to print please seek out a member of staff" << std::endl;
+   }
+  }
+  else if (money<0)
+  {
+   std::cout << "That is not a figure than can be withdraw please use whole positive numbers" << std::endl;
+   std::cout << "The withdrawal has been aborted" << std::endl;
+  }
+  else if (money>deposit)
+  {
+   std::cout << "You don't have that much money" << std::endl;
+   std::cout << "The withdrawal has been aborted" << std::endl;
+  }
+ }
+ else
+ {
+  if (money <= deposit && money >= 0)
+  {
+   deposit -= money;
+   // accountFile<<deposit;
+  }
+  else if (money<0)
+  {
+   std::cout << "That is not a figure than can be withdraw please use whole positive numbers" << std::endl;
+   std::cout << "The withdrawal has been aborted" << std::endl;
+  }
+  else if (money>deposit)
+  {
+   std::cout << "You don't have that much money" << std::endl;
+   std::cout << "The withdrawal has been aborted" << std::endl;
+  }
+ }
 
-receiptFile.close();
-//accountFile.close();
+ receiptFile.close();
+ //accountFile.close();
 
+}
+
+int main()
+{
+ Bank_Account test;
+ return 0;
 }
