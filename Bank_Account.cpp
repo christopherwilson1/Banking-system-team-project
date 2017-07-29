3a57c57c8caedd56900d038f483b92a42450c4ca
christopherwilson1 <wilso235@uni.coventry.ac.uk>
2017-07-29 14:19:37 +0100

    Added the bank withdrawala
    added the bank withdrawal function class and changed the class creation txt doc save to a function in main still working on both right now though

--- Bank_Account.cpp
+++ Bank_Account.cpp
@@ -1,5 +1,3 @@
-#include "Bank_Account.h"
-
 Bank_Account::Bank_Account()
 {
     //ctor
@@ -15,25 +13,87 @@ void Bank_Account::account_creation()
     std::getline(std::cin,customer_name);
     std::cout<<"Enter in the account number"<<std::endl;
     std::cin>>account_num;
-    std::cout<<"enter in the account type"<<std::endl;
+    std::cout<<"enter in the account type current or savings"<<std::endl;
     std::cin>>account_type;
     std::cout<<"enter in the amount you would like to put in your account"<<std::endl;
     std::cin>>deposit;
-    std::cout<<"congratulations your account has been created"<<std::endl;
-    std::ofstream accountFile;
+    std::cout<<"congratulations your account has been created saving information"<<std::endl;
+   /* std::ofstream accountFile;
     accountFile.open("customer_account.txt");
     if(accountFile.is_open())
             {
                 std::cout<<"account saved"<<std::endl;
-                accountFile<<customer_name;
-                accountFile<<account_num
-                accountFile<<account_type
-                accountFile<<deposit
+                accountFile<<customer_name<<std::endl;
+                accountFile<<account_num<<std::endl;
+                accountFile<<account_type<<std::endl;
+                accountFile<<deposit<<std::endl;
 
             }
             else
             {
                 std::cout<<"information failed to save please seek out a member of staff"<<std::endl;
             }
-accountFile.close();
+accountFile.close();*/
+}
+
+void Bank_Account::withdraw_money(double money)
+{
+    string answer;
+    std::cout<<"would you like a receipt y/n"<<std::endl;
+
+    std::cin>>answer;
+    std::ofstream receiptFile;
+    receiptFile.open("customer_receipt.txt");
+    //std::ofstream accountFile;
+    //accountFile.open("customer_account.txt");
+    if(answer=="y")
+    {
+
+        if(money<=deposit && money >=0 )
+        {
+            deposit-=money;
+            if(receiptFile.is_open())
+            {
+                std::cout<<"receipt printed"<<std::endl;
+                receiptFile<<deposit<<std::endl;
+                //accountFile<<deposit<<std::endl;
+            }
+            else
+            {
+                std::cout<<"receipt failed to print please seek out a member of staff"<<std::endl;
+            }
+        }
+        else if (money<0)
+        {
+            std::cout<<"That is not a figure than can be withdraw please use whole positive numbers"<<std::endl;
+            std::cout<<"The withdrawal has been aborted"<<std::endl;
+        }
+        else if(money>deposit)
+        {
+            std::cout<<"You don't have that much money"<<std::endl;
+            std::cout<<"The withdrawal has been aborted"<<std::endl;
+        }
+    }
+    else
+    {
+        if(money<=deposit && money >=0 )
+        {
+            deposit-=money;
+           // accountFile<<deposit;
+        }
+        else if (money<0)
+        {
+            std::cout<<"That is not a figure than can be withdraw please use whole positive numbers"<<std::endl;
+            std::cout<<"The withdrawal has been aborted"<<std::endl;
+        }
+        else if(money>deposit)
+        {
+            std::cout<<"You don't have that much money"<<std::endl;
+            std::cout<<"The withdrawal has been aborted"<<std::endl;
+        }
+    }
+
+receiptFile.close();
+//accountFile.close();
+
 }
