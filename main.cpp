1022a7a1f569aa6dcda6d0ff591c5efcc100f96b
christopherwilson1 <wilso235@uni.coventry.ac.uk>
2017-08-21 20:06:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,193 +1,158 @@
-#include <iostream>
-#include<string>
+
+#include<iostream>
 #include<fstream>
-#include<iomanip>
-#include "Bank_Account.h"
-#include <sstream>
-#include <cstdlib>
 #include<cctype>
-#include<stdio.h>
+#include<iomanip>
 #include<conio.h>
-#include<ctype.h>
+#include<stdio.h>
+#include"bank_Account.h"
 using namespace std;
-void menu();
-void build_account();
-void read_file(int a);
-void deposite_and_withdraw(int a,int choice);
+
+
+
+
+
+void write_account();//writes the account to a txt doc
+void display_info(int);//take the info from txt doc and displays it (just 1 user)
+void deposit_withdraw(int, int);//handles with draw and deposit
+
+
+
 int main()
 {
- int console_input;
- char button;
+	char button;
+	int console_input;
 
- do{
-
+	do
+	{
     cout << "      Menu (Select option or enter 5 to quit)" << endl;
     cout << "1. New Account "<< endl;
     cout << "2. Deposit Cash "<< endl;
     cout << "3. Withdraw Cash"<< endl;
     cout << "4. Balance Enquiry"<< endl;
+    cout << "5. To exit "<< endl;
     cout << "Enter option:";
-    cin>>button;
 
-    switch(button)
-    {
-     case '1':
-            build_account();
+		cin>>button;
+		switch(button)
+		{
+		 case '1':
+            write_account();
             break;
-     case '2':
+        case '2':
             cout<<"enter in your account number"<<endl;
             cin>>console_input;
-            deposite_and_withdraw(console_input,1);
+            deposit_withdraw(console_input,1);
             break;
-     case '3':
+        case '3':
             cout<<"enter in your account number"<<endl;
             cin>>console_input;
-            deposite_and_withdraw(console_input,2);
+            deposit_withdraw(console_input,2);
             break;
-     case '4':
+        case '4':
             cout<<"enter in your account number"<<endl;
             cin>>console_input;
-            read_file(console_input);
+            display_info(console_input);
             break;
-     case '5':
+        case '5':
         cout<<"Thank you for using the system, we hope you enjoyed it"<<endl;
      default :cout<<"\a";
-    }
-    getch();
-}while(button!='5');
-return 0;
-
+		}
+		getch();
+	}while(button!='5');
+	return 0;
 }
 
 
 
-
-
-
-
-
-
-/*void menu() {
- cout << "      Menu (Select option or enter 5 to quit)" << endl;
- cout << "1. New Account "<< endl;
- cout << "2. Deposit Cash "<< endl;
- cout << "3. Withdraw Cash"<< endl;
- cout << "4. Balance Enquiry"<< endl;
- cout << "Enter option:";
-
- int input;
- cin >> input;
-
- if (input == 1) {
- build_account();
- }
- else if (input == 2) {
-  money_in_out(input,1);
- }
-
- else if (input == 3)
- {
-money_in_out(input,1);
-}
- else if (input == 4) {
-  read_file(input);
- }
- else if (input== 5){
-    exit(0);
- }
- else {
-    cout<<  "invalid input";
- }
-}*/
-//this function writes the account details into a file
-void build_account()
+void write_account()
 {
-    Bank_Account customer;
-    ofstream enteraccountFile;
-    enteraccountFile.open("customer_account.dat",ios::binary|ios::app);
-    customer.account_creation();
-    enteraccountFile.write((char *)&customer, sizeof(Bank_Account));
-    enteraccountFile.close();
+	bank_Account customer;
+	ofstream customerFile;
+	customerFile.open("customer_account.txt",ios::app);
+	customer.account_creation();
+	customerFile.write((char *) &customer, sizeof(bank_Account));
+	customerFile.close();
 }
 
 
-
-//this function reads the file
-void read_file(int a)
+void display_info(int num)
 {
-    Bank_Account customer;
-    ifstream readaccountFile;
-    readaccountFile.open("customer_account.dat",ios::binary);
-    bool flag=false;
-    if(!readaccountFile)
-    {
-        cout<<"File could not be found or opened please contact a member of staff"<<endl;
-        return;
-    }
-    cout<<"    Balance Information    "<<endl;
-    while(readaccountFile.read((char *) &customer,sizeof(Bank_Account)))
-    {
-        if(customer.give_accountnum()==a)
-        {
-            customer.show_account();
-            flag=true;
-        }
-    }
-    readaccountFile.close();
-    if(flag==false)
-        cout<<"This account does not exist"<<endl;
-
+	bank_Account customer;
+	int flag=0;
+	ifstream customerFile;
+	customerFile.open("customer_account.txt");
+	if(!customerFile)
+	{
+		cout<<"File did not open please seek assistance from staff ";
+		return;
+	}
+	cout<<"Balance Information"<<endl;
+	while(customerFile.read((char *) &customer, sizeof(bank_Account)))
+	{
+		if(customer.ret_account_number()==num)
+		{
+			customer.show_account();
+			flag=1;
+		}
+	}
+	customerFile.close();
+	if(flag==0)
+		cout<<"Could not find account number please talk to staff";
 }
 
 
-
-void deposite_and_withdraw(int a,int choice)
+void deposit_withdraw(int n, int option)
 {
-    int amount;
-    int found=0;
-    Bank_Account customer;
-    fstream accountFile;
-    accountFile.open("customer_account.dat",ios::binary|ios::in|ios::out);
-    if(!accountFile)
-    {
-        cout<<"ERROR! File could not be opened"<<endl;
-        return;
-    }
-    while(accountFile.read((char*)&customer,sizeof(Bank_Account))&&found==0)
-    {
-        if(customer.give_accountnum()==a)
-        {
-            customer.show_account();
-            if(choice==1)//handles deposits
-            {
-                cout<<"enter in the amount you would like to deposit"<<endl;
-                cin>>amount;
-                customer.deposit_money(amount);
-            }
-            if(choice==2)//handles withdraws
-            {
-                cout<<"enter in the amount you would like to withdraw"<<endl;
-                cin>>amount;
-                int balance=customer.give_accountdep()-amount;
-                if((balance<500&&customer.give_accounttype()=='s')||(balance<1000&&customer.give_accounttype()=='c'))
-                    cout<<"not enough money"<<endl;
-                else
-                    customer.withdraw_money(amount);
-            }
-            int position=(-1)*sizeof(customer);
-            accountFile.seekp(position,ios::cur);
-            accountFile.write((char*)&customer,sizeof(Bank_Account));
-            cout<<"Information has been saved"<<endl;
-            found=1;
+	int amount;
+	int found=0;
+	bank_Account customer;
+	fstream customerFile;
+	customerFile.open("customer_account.txt",ios::in|ios::out);
+	if(!customerFile)
+	{
+		cout<<"File did not open please seek assistance from staff";
+		return;
+	}
+	while(customerFile.read((char *) &customer, sizeof(bank_Account)) && found==0)
+	{
+	    int receipt_opt;
+		if(customer.ret_account_number()==n)
+		{
+			customer.show_account();
+			if(option==1)
+			{
+				cout<<"    Deposit money    "<<endl;
+				cout<<"Enter the amount you would like to deposit: "<<endl;
+				cin>>amount;
+				customer.deposit(amount);
 
-        }
-    }
-    accountFile.close();
-    if(found==0)
-        cout<<"account could not be found please make one"<<endl;
+			}
+			if(option==2)
+			{
+				cout<<"   Withdraw Screen  "<<endl;
+				cout<<"Enter The amount to be withdraw: "<<endl;
+				cin>>amount;
+				int bal=customer.ret_balance();
+				if(bal<amount)
+					cout<<"Not enough money";
+				else
+					customer.withdraw(amount);
 
-
+			}
+			int pos=(-1)* sizeof(customer);
+			customerFile.seekp(pos,ios::cur);
+			customerFile.write((char *) &customer, sizeof(bank_Account));
+			//cout<<"Information saved";
+			found=1;
+	       }
+	}
+	customerFile.close();
+	if(found==0)
+		cout<<"\n\n Record Not Found ";
 }
+
+
 
 
 
